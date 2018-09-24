2d5c8d53f1ec1a107bf4264bf7a6b33bbb4ba1d2
Nursultan Makhamatov <zeropulse8@gmail.com>
2018-09-24 23:13:01 +0300

    done

--- 2.JavaCore/src/com/javarush/task/task20/task2013/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2013/Solution.java
@@ -1,16 +1,14 @@
 package com.javarush.task.task20.task2013;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /* 
 Externalizable Person
 */
 public class Solution {
-    public static class Person {
+    public static class Person implements Externalizable {
         private String firstName;
         private String lastName;
         private int age;
@@ -18,6 +16,7 @@ public class Solution {
         private Person father;
         private List<Person> children;
 
+        public Person(){}
         public Person(String firstName, String lastName, int age) {
             this.firstName = firstName;
             this.lastName = lastName;
@@ -40,24 +39,65 @@ public class Solution {
         public void writeExternal(ObjectOutput out) throws IOException {
             out.writeObject(mother);
             out.writeObject(father);
-            out.writeChars(firstName);
-            out.writeChars(lastName);
+            out.writeBytes(firstName + "\n");
+            out.writeBytes(lastName + "\n");
             out.writeInt(age);
             out.writeObject(children);
+            out.flush();
         }
 
         @Override
         public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            setMother((Person)in.readObject());
+            System.out.println("mother = " + mother +"\n");
+//            mother = (Person)in.readObject();
+            setFather((Person)in.readObject());
+            System.out.println("father = " + father +"\n");
+
+//            father = (Person)in.readObject();
             firstName = in.readLine();
+            System.out.println("firstName = " + firstName + "\n");
+
             lastName = in.readLine();
-            father = (Person)in.readObject();
-            mother = (Person)in.readObject();
+            System.out.println("lastName = " + lastName + "\n");
+
             age = in.readInt();
-            children = (List)in.readObject();
+            setChildren((List<Person>)in.readObject());
+//            children = (List<Person>)in.readObject();
+        }
+
+        @Override
+        public String toString() {
+            return "name = " + firstName + "\n surname = " + lastName + "\n age = " + age + "\n mother = " + mother + "\n father = " + father
+                    + "\n children = " + children;
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
+        Person mother = new Person("mama","momi", 103);
+        Person father = new Person("fatha", "Fathov",120);
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("chidy", "fridy", 10));
+
+
+        String file = "C:\\Users\\Nursultan\\Desktop\\Java\\IO\\in.txt";
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        Person person = new Person("Name","SurName",88);
+        person.setMother(mother);
+        person.setFather(father);
+        person.setChildren(list);
+
+        objectOutputStream.writeObject(person);
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        Person readPerson = (Person)objectInputStream.readObject();
+        System.out.println("==============PERSON=============\n" + person);
+        System.out.println("==============READPERSON=============\n" + readPerson);
+        objectOutputStream.close();
+        objectInputStream.close();
 
     }
 }
