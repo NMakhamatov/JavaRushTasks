57da50a12d479636e805450b83ad5b33037035e9
Nursultan Makhamatov <zeropulse8@gmail.com>
2019-05-09 19:24:48 +0300

    added tasks

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task35.task3502;
+
+/* 
+Знакомство с дженериками
+*/
+//todo to do
+public class Solution {
+    public static class SomeClass {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
